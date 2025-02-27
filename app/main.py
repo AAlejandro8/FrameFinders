f7723130e27f2092549c2df28e6c1b297df4120c
Alejandro Albarran <115290483+AAlejandro8@users.noreply.github.com>
2025-02-27 18:01:39 -0500

    added template and trending movies

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import os
+import requests
 
+api_key = os.environ["TMD_API_KEY"]
+read_key = os.environ["TMD_READ_ACCESS_KEY"]
+headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {read_key}"
+      }
+
+def get_trending_movies():
+      url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
+      response = requests.get(url, headers=headers)
+      return response
+      
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods = ["GET", "POST"])
 def home():
-    request = request.get()
-    return jsonify(request.json())
+      trending = get_trending_movies()
+      
+      return render_template("index.html", movies=trending.json()["results"])
