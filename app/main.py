71c8c3695918faa0461ea72da6a6a3c643dd9b3d
Alejandro Albarran <115290483+AAlejandro8@users.noreply.github.com>
2025-02-28 15:28:48 -0500

    Added genre indicatiors for movies and search functionality!

--- app/main.py
+++ app/main.py
@@ -13,11 +13,40 @@ def get_trending_movies():
       url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
       response = requests.get(url, headers=headers)
       return response
+
+def get_movies_with_genre(movies):
+      genres = "https://api.themoviedb.org/3/genre/movie/list"
+      response = requests.get(genres, headers=headers)
+      genre_id_list = response.json()["genres"]
+      
+      # map genre ids to genre names
+      genre_map = {genre["id"]: genre["name"] for genre in genre_id_list}
+      
+      movie_result = movies.json()["results"]
+
+      # add genre names to each movie
+      for movie in movie_result:
+            movie_genre_ids = movie.get("genre_ids", [])
+            movie["genre_names"] = [genre_map.get(genre_id, "Unknown") for genre_id in movie_genre_ids]
+
+      return movie_result
       
 app = Flask(__name__)
 
 @app.route("/",methods = ["GET", "POST"])
 def home():
       trending = get_trending_movies()
+      movies_with_genre = get_movies_with_genre(trending)
       
-      return render_template("index.html", movies=trending.json()["results"])
+      return render_template("index.html",
+                             movies=movies_with_genre)
+
+@app.route("/search", methods = ["GET"])
+def search():
+      query = request.args.get("q", "")
+      url = f'https://api.themoviedb.org/3/search/movie?query={query}&include_adult=false&language=en-US&page=1'
+      response = requests.get(url, headers=headers)
+      movies = get_movies_with_genre(response)
+      
+      return render_template("search.html", 
+                             movies=movies)
