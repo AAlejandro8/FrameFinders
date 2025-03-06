9c4e297e8ab76c90706db216708d6a3b6aaefad5
Alejandro Albarran <115290483+AAlejandro8@users.noreply.github.com>
2025-03-05 22:12:50 -0500

    added db

--- app/models.py
+++ app/models.py
@@ -1,2 +1,8 @@
 from datetime import datetime
 from app import db
+
+class WatchList(db.model):
+      id = db.Column(db.Integer, primary_key=True)
+      movie_id = db.Column(db.Integer, unique=True, nullable=False)  # TMDB ID
+      title = db.Column(db.String(200), nullable=False)
+      added_at = db.Column(db.DateTime, default=db.func.current_timestamp())
